a4ecee7922210809d106c88465cd8da0f0dc11d2
edgarN <edgare.najar@gmail.com>
2023-05-20 21:45:01 -0600

    changes in misra and state machine

--- app/app_clock.c
+++ app/app_clock.c
@@ -7,25 +7,28 @@
  *
  * @note    None
  */
- 
 #include "app_bsp.h"
 #include "app_clock.h"
 
 /**
  * @brief  Structure type variable to initialize the RTC
  */
-
-static RTC_HandleTypeDef hrtc = {0};
+static RTC_HandleTypeDef hrtc = {num_0};
 
 /**
  * @brief  Variable to control changes in time data
  */
-
 uint8_t changes = WAIT_MESSAGE;
 
+/**
+ * @brief  To store milisecods for display
+ */
 static uint32_t tick_display;
 
-static uint8_t default_data = 1u;
+/**
+ * @brief  To display the default date and time
+ */
+static uint8_t default_data = num_1;
 
 /**
  * @brief   **Initialization of the RTC**
@@ -35,18 +38,15 @@ static uint8_t default_data = 1u;
  *
  * @param   hrtc [out] Structure type variable used to configure the RTC
  *
- * @retval  None
- *
  * @note None
  */
-
 void Clock_Init( void )
 {
     /* Configuration RTC */
     hrtc.Instance             = RTC;
     hrtc.Init.HourFormat      = RTC_HOURFORMAT_24;
-    hrtc.Init.AsynchPrediv    = 127;
-    hrtc.Init.SynchPrediv     = 255;
+    hrtc.Init.AsynchPrediv    = val_AsynchPrediv;
+    hrtc.Init.SynchPrediv     = val_SynchPrediv;
     hrtc.Init.OutPut          = RTC_OUTPUT_DISABLE;
     /* Initilize the RTC with 24 hour format and no output signal enble */
     HAL_RTC_Init( &hrtc );
@@ -61,25 +61,22 @@ void Clock_Init( void )
  *
  * @param   changes [out] To control changes in time data
  *
- * @retval  None
- *
  * @note None
  */
-
 void Clock_Task( void )
 {
-    RTC_DateTypeDef sDate  = {0};
-    RTC_TimeTypeDef sTime  = {0};
-    
+    RTC_DateTypeDef  sDate  = {num_0};
+    RTC_TimeTypeDef  sTime  = {num_0};
+    RTC_AlarmTypeDef sAlarm = {num_0};
 
-    if( default_data == 1u )
+    if( default_data == num_1 )
     {
         default_data++;
         /* Setting default time at 23:59:50 in BCD format */
-        sTime.Hours   = 0x23;
-        sTime.Minutes = 0x59;
-        sTime.Seconds = 0x50;
-        sTime.SubSeconds = 0x00;
+        sTime.Hours   = def_Hours;
+        sTime.Minutes = def_Minutes;
+        sTime.Seconds = def_Seconds;
+        sTime.SubSeconds = def_SubSeconds;
         sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
         sTime.StoreOperation = RTC_STOREOPERATION_RESET;
         HAL_RTC_SetTime( &hrtc, &sTime, RTC_FORMAT_BCD );
@@ -87,28 +84,30 @@ void Clock_Task( void )
         /* Setting default date at Monday January 31, 2023 in BCD format */
         sDate.WeekDay = RTC_WEEKDAY_TUESDAY;
         sDate.Month = RTC_MONTH_JANUARY;
-        sDate.Date = 0x31;
-        sDate.Year = 0x23;
-        ClockMsg.tm.tm_yday = 0x20;
+        sDate.Date = def_Date;
+        sDate.Year = def_YearLSB;
+        ClockMsg.tm.tm_yday = def_YearMSB;
         HAL_RTC_SetDate( &hrtc, &sDate, RTC_FORMAT_BCD );
     }
+    else
+    {}
 
     switch( changes )
     {
         case WAIT_MESSAGE:
-            if( MSGHandler.msg == (uint8_t)CHANGE_TIME )
+            if( MSGHandler.msg == CHANGE_TIME )
             {
                 changes = CHANGE_TIME;
             }
-            else if( MSGHandler.msg == (uint8_t)CHANGE_DATE )
+            else if( MSGHandler.msg == CHANGE_DATE )
             {
                 changes = CHANGE_DATE;
             }
-            else if( MSGHandler.msg == (uint8_t)CHANGE_ALARM )
+            else if( MSGHandler.msg == CHANGE_ALARM )
             {
                 changes = CHANGE_ALARM;
             }
-            else if( (HAL_GetTick() - tick_display) >= 1000 )
+            else if( (HAL_GetTick() - tick_display) >= one_second )
             {
                 tick_display = HAL_GetTick();
                 changes = DISPLAY;
@@ -125,26 +124,35 @@ void Clock_Task( void )
             sTime.Hours   = MSGHandler.tm.tm_hour;
             sTime.Minutes = MSGHandler.tm.tm_min;
             sTime.Seconds = MSGHandler.tm.tm_sec;
-            sTime.SubSeconds = 0x00;
-            sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
-            sTime.StoreOperation = RTC_STOREOPERATION_RESET;
             HAL_RTC_SetTime( &hrtc, &sTime, RTC_FORMAT_BCD );
             break;
 
         case CHANGE_DATE:
             changes = DISPLAY;
+            
             ClockMsg.tm.tm_yday = MSGHandler.tm.tm_yday;
             /* Setting date in BCD format */
             sDate.WeekDay = MSGHandler.tm.tm_wday;
-            sDate.Month = MSGHandler.tm.tm_mon;
-            sDate.Date = MSGHandler.tm.tm_mday;
-            sDate.Year = MSGHandler.tm.tm_year;
+            sDate.Month   = MSGHandler.tm.tm_mon;
+            sDate.Date    = MSGHandler.tm.tm_mday;
+            sDate.Year    = MSGHandler.tm.tm_year;
             /* Set date */
             HAL_RTC_SetDate( &hrtc, &sDate, RTC_FORMAT_BCD );
             break;
 
         case CHANGE_ALARM:
             changes = DISPLAY;
+
+            sAlarm.AlarmTime.Hours   = 0x12;
+            sAlarm.AlarmTime.Minutes = 0x12;
+            sAlarm.AlarmTime.Seconds = 0x12;
+            sAlarm.AlarmTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+            sAlarm.AlarmTime.StoreOperation = RTC_STOREOPERATION_RESET;
+            sAlarm.AlarmSubSecondMask = RTC_ALARMSUBSECONDMASK_ALL;
+            sAlarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_DATE;
+            sAlarm.AlarmDateWeekDay = 0x12;
+            sAlarm.AlarmMask = RTC_ALARMMASK_NONE;
+            sAlarm.Alarm = RTC_ALARM_A;
             break;
 
         case DISPLAY:
--- app/app_clock.h
+++ app/app_clock.h
@@ -6,27 +6,36 @@
  *
  * @note    none
  */
- 
 #ifndef APP_CLOCK_H__
 #define APP_CLOCK_H__
 
 /**
  * @brief  to determine the type of change to do
  */
-
 extern uint8_t changes;
 
-/**
-  * @brief   enum for change option
-  */
+#define WAIT_MESSAGE (uint8_t)0
+#define CHANGE_TIME  (uint8_t)1
+#define CHANGE_DATE  (uint8_t)2
+#define CHANGE_ALARM (uint8_t)3
+#define DISPLAY      (uint8_t)4
 
-typedef enum{
-    WAIT_MESSAGE,
-    CHANGE_TIME,
-    CHANGE_DATE,
-    CHANGE_ALARM,
-    DISPLAY
-}change_msn;
+#define val_AsynchPrediv 127
+#define val_SynchPrediv  255
+
+#define def_Hours      0x23
+#define def_Minutes    0x59
+#define def_Seconds    0x50
+#define def_SubSeconds 0x00
+
+#define def_Date    0x31
+#define def_YearLSB 0x23
+#define def_YearMSB 0x20
+
+#define one_second 1000
+
+#define num_0 (uint8_t)0
+#define num_1 (uint8_t)1
 
 void Clock_Init( void );
 void Clock_Task( void );
